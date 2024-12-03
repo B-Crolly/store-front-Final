862cfc5994adf34e05e030116dd7cba75aed70e8
B-Crolly <crol0005@algonquinlive.com>
2024-12-03 18:32:48 -0500

    another submodule test

--- src/App.vue
+++ src/App.vue
@@ -109,7 +109,7 @@ export default {
 
 <style>
 body {
-  background-image: url('@/assets/best-buy-background.jpg'); /* added bestbuy background test1 */
+  background-image: url('@/assets/best-buy-background.jpg'); /* added bestbuy background test2 */
   background-size: cover;
   background-position: center;
   background-attachment: fixed; /* Keeps the background in place when scrolling */
